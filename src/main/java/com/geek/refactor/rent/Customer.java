daf32259544c4414d5994634260b7c2a43d2a049
liuxiaocheng <xiaocheng.liu0902@gmail.com>
2019-03-12 14:25:34 +0800

    refactor v5
    Extract Method getFrequentRenterPoints

--- src/main/java/com/geek/refactor/rent/Customer.java
+++ src/main/java/com/geek/refactor/rent/Customer.java
@@ -37,11 +37,7 @@ public class Customer {
 
             Rental each = rentals.next();
 
-            frequentRenterPoints++;
-
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
-                frequentRenterPoints++;
-            }
+            frequentRenterPoints += each.getFrequentRenterPoints();
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
             totalAmount += each.getCharge();
         }
--- src/main/java/com/geek/refactor/rent/Rental.java
+++ src/main/java/com/geek/refactor/rent/Rental.java
@@ -57,4 +57,13 @@ public class Rental {
         }
         return thisAmount;
     }
+
+    public int getFrequentRenterPoints() {
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+
+    }
 }
