b7dc437b7e931bae04aa44cfead84482366e9bc0
liuxiaocheng <xiaocheng.liu0902@gmail.com>
2019-03-12 11:47:02 +0800

    refactor v3
    Move Method "amountFor" from Customer to Rental

--- src/main/java/com/geek/refactor/rent/Customer.java
+++ src/main/java/com/geek/refactor/rent/Customer.java
@@ -37,7 +37,7 @@ public class Customer {
             double thisAmount = 0;
             Rental each = rentals.next();
 
-            thisAmount = amountFor(each);
+            thisAmount = each.amountFor();
 
             frequentRenterPoints++;
 
@@ -52,27 +52,4 @@ public class Customer {
         return result;
     }
 
-    private double amountFor(Rental aRental) {
-        double thisAmount = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (aRental.getDaysRented() > 2) {
-                    thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (aRental.getDaysRented() > 3) {
-                    thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-                }
-                break;
-            default:
-                thisAmount = 0;
-        }
-        return thisAmount;
-    }
 }
--- src/main/java/com/geek/refactor/rent/Rental.java
+++ src/main/java/com/geek/refactor/rent/Rental.java
@@ -33,4 +33,28 @@ public class Rental {
     public void setDaysRented(int daysRented) {
         this.daysRented = daysRented;
     }
+
+    public double amountFor() {
+        double thisAmount = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (getDaysRented() > 2) {
+                    thisAmount += (getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (getDaysRented() > 3) {
+                    thisAmount += (getDaysRented() - 3) * 1.5;
+                }
+                break;
+            default:
+                thisAmount = 0;
+        }
+        return thisAmount;
+    }
 }
