0f1e98c46182ee24bf777407304b1ffa588b8710
liuxiaocheng <xiaocheng.liu0902@gmail.com>
2019-03-13 11:27:51 +0800

    refactor v7
    Move Method getCharge and getFrequentRentalPoints from Rental to Movie

--- src/main/java/com/geek/refactor/rent/Movie.java
+++ src/main/java/com/geek/refactor/rent/Movie.java
@@ -36,4 +36,37 @@ public class Movie {
     public void setPriceCode(int priceCode) {
         this.priceCode = priceCode;
     }
+
+    public double getCharge(int daysRented) {
+        double thisAmount = 0;
+        switch (getPriceCode()) {
+            case REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2) {
+                    thisAmount += (daysRented - 2) * 1.5;
+                }
+                break;
+            case NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3) {
+                    thisAmount += (daysRented - 3) * 1.5;
+                }
+                break;
+            default:
+                thisAmount = 0;
+        }
+        return thisAmount;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((this.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+
+    }
 }
--- src/main/java/com/geek/refactor/rent/Rental.java
+++ src/main/java/com/geek/refactor/rent/Rental.java
@@ -34,36 +34,12 @@ public class Rental {
         this.daysRented = daysRented;
     }
 
-    public double getCharge() {
-        double thisAmount = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (getDaysRented() > 2) {
-                    thisAmount += (getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (getDaysRented() > 3) {
-                    thisAmount += (getDaysRented() - 3) * 1.5;
-                }
-                break;
-            default:
-                thisAmount = 0;
-        }
-        return thisAmount;
+    public int getFrequentRenterPoints() {
+        return this.getMovie().getFrequentRenterPoints(this.getDaysRented());
+
     }
 
-    public int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
-
+    public double getCharge() {
+        return this.getMovie().getCharge(this.getDaysRented());
     }
 }
